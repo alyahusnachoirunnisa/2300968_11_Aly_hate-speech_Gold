6fca1a55242c97ef7c668267a48f044910dd72b6
Alya Husna <husnachoirunnisaa@gmail.com>
2023-07-29 09:20:35 +0700

    update file

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
+
 app = FastAPI()
 
 @app.get("/")
 async def index():
-    return JSONResponse(
-        content = {
-            "ok" : True,
-            "code" : 200,
-            "data" : {
-                "version" : "1.0.0"
-            },
-            "message" : "Success"
-        }
-    )
+    content = {
+        "ok" : True,
+        "code" : 200,
+        "data" : {
+            "version" : "1.0.0"
+        },
+        "message" : "Success"
+    }
+
+    return JSONResponse(content)
+
+   
 
 from routers import cleansing
 from routers import sentiment
+from routers import database
 
 app.include_router(cleansing.router, tags=["Cleansing API"])
 app.include_router(sentiment.router, tags=["Sentiment API"])
+app.include_router(database.router, tags=["Database API"])
 
-# ada perubahan
